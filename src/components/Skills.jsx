cca96359f8fa5c48d3faf92d5fcfc73866b075c4
Andrea Morshead <andrea.morshead@gmail.com>
2025-01-02 15:53:14 -0400

    updated skills colours

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,18 +2,18 @@ import Skill from "./Skill";
 
 export default function Skills() {
     const skills = [
-        { name: "JavaScript", color: "#FFEE93" }, // pastel yellow
-        { name: "Java", color: "#FFADAD" },       // pastel red
-        { name: "HTML/CSS", color: "#B9FBC0" },  // pastel green
-        { name: "PHP", color: "#A0C4FF" },       // pastel blue
-        { name: "MongoDB", color: "#FFCFD2" },   // pastel pink
-        { name: "SQL", color: "#C4B7E7" },       // pastel purple
-        { name: "React", color: "#FFD6A5" },     // pastel peach
-        { name: "Laravel", color: "#D0F4DE" },   // pastel mint
-        { name: "Wordpress", color: "#FDE2E4" }, // pastel rose
-        { name: "Git", color: "#F1C0E8" },       // pastel lavender
-        { name: "Tailwind", color: "#FAE3D9" },  // pastel cream
-        { name: "Bootstrap", color: "#BDE0FE" }, // pastel sky
+        { name: "JavaScript", color: "#FFD700" }, 
+        { name: "Java", color: "#FF0598" },      
+        { name: "HTML/CSS", color: "#ACF39D" },   
+        { name: "PHP", color: "#A0C4FF" },       
+        { name: "MongoDB", color: "#FFCFD2" },    
+        { name: "SQL", color: "#EE70FF" },       
+        { name: "React", color: "#EE7B30" },     
+        { name: "Laravel", color: "#36C9C6" },    
+        { name: "Wordpress", color: "#EDFF86" },  
+        { name: "Git", color: "#F1C0E8" },        
+        { name: "Tailwind", color: "#D9FFF8" },   
+        { name: "Bootstrap", color: "#51E5FF" },  
     ];
 
     return (
